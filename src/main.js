3a7c5b8e6ce44f0d2407db63e8ac1a0fcb675521
Atabek Kurbanov <yuzeyzer.atabek@gmail.com>
2024-08-18 06:40:55 +0600

    dasdsadd

--- src/main.js
+++ src/main.js
@@ -70,8 +70,8 @@ processQueue.process(async (job) => {
 
   try {
     for (const msgData of messages) {
-      console.log(`Обработка сообщения: ${JSON.stringify(msgData.id)}`);
-      const msg = await CLIENT.getMessageById(String(msgData.id))
+      console.log(`Обработка сообщения: ${JSON.stringify(msgData.id._serialized)}`);
+      const msg = await CLIENT.getMessageById(String(msgData.id._serialized))
       await onMessageCreated(msg);
     }
   } catch (err) {
