8029e3709f945bbaf50ad35c8ff87e52bf8d48e8
Atabek Kurbanov <yuzeyzer.atabek@gmail.com>
2024-08-21 13:50:54 +0600

    changed text

--- src/whatsapp-dordoi-bot/actions.js
+++ src/whatsapp-dordoi-bot/actions.js
@@ -340,7 +340,7 @@ const botSettingsActions = async (msg) => {
     try {
       await CLIENT.sendMessage(
         BOT_SETTINGS_GROUP.ID,
-        `Имя: Dordoi Killer\nДата Создания: 05.08.2024\nКонфигурация Сервера: 3 CPU, 4GB RAM, 15GB SSD\nКоличество чатов куда рассылаются сообщения: ${botSourceChatsCounter}\nКоличество чатов откуда берутся сообщения: ${botDestChatsCounter}\n
+        `Имя: Dordoi Killer\nДата Создания: 05.08.2024\nКонфигурация Сервера: 3 CPU, 4GB RAM, 15GB SSD\nКоличество чатов куда рассылаются сообщения: ${botDestChatsCounter}\nКоличество чатов откуда берутся сообщения: ${botSourceChatsCounter}\n
         `
       );
     } catch (err) {
