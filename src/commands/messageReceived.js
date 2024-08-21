343030f34758dbfb4925909cd8c64792cb62ae1f
Atabek Kurbanov <yuzeyzer.atabek@gmail.com>
2024-08-21 10:44:48 +0600

    increased interval

--- src/commands/messageReceived.js
+++ src/commands/messageReceived.js
@@ -132,7 +132,7 @@ const messageReceived = async (msg) => {
 
   debouncedMessages();
 };
-const debouncedMessages = debounce(sendMessagesFromGroups, 90000);
+const debouncedMessages = debounce(sendMessagesFromGroups, 180000);
 
 async function sendMessagesFromGroups() {
   if (groupsQueueFlag) return;
